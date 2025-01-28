b2ac80c2e70cc854a92bf4ca64765261e012ed7e
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-01-28 22:02:26 +0200

    1

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { Header } from "../components/Header.jsx";
 import { useEffect } from "react";
 import { getCampers } from "../redux/campers/operations";
-import { CampersList } from "../components/CampersList/CampersList";
+import { CampersList } from "../components/CampersList/CampersList.jsx";
 
 export const Catalog = () => {
   const dispatch = useDispatch();
